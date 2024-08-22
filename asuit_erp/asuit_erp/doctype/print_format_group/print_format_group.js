1d144fc6c6d1b9ebd79cd13232d68c178e240dc1
baodawei <baodawei@me.com>
2024-08-22 18:14:28 +0900

    Change

--- asuit_erp/asuit_erp/doctype/print_format_group/print_format_group.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, ASUIT Inc and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Print Format Group", {
-// 	refresh(frm) {
-
-// 	},
-// });
