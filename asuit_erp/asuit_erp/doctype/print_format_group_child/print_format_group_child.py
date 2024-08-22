8887014ba0192eeec48c85ed3d8a656794cfc2ed
baodawei <baodawei@me.com>
2024-08-22 17:25:28 +0900

    Change

--- asuit_erp/asuit_erp/doctype/print_format_group_child/print_format_group_child.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, ASUIT Inc and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class PrintFormatGroupChild(Document):
-	pass
