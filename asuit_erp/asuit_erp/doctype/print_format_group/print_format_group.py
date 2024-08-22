0520d535e2dd51dbb7e53cc73ee618a5011af61a
baodawei <baodawei@me.com>
2024-08-22 17:23:07 +0900

    Change

---
+++ asuit_erp/asuit_erp/doctype/print_format_group/print_format_group.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ASUIT Inc and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PrintFormatGroup(Document):
+	pass
